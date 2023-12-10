822c05bbd70c0f5bcd8f0162940e8ce1cb3163f2
rhmnaulia <rahmanauliax@gmail.com>
2023-12-10 19:18:04 +0700

    chore: remove unused

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { GeistSans, GeistMono } from 'geist/font'
+import { GeistSans } from 'geist/font'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'

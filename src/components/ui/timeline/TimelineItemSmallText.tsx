767bfb64f7b44a33811afe3b202b7f3744aa7815
rhmnaulia <rahmanauliax@gmail.com>
2024-03-05 15:05:00 +0700

    delete all codebase

--- src/components/ui/timeline/TimelineItemSmallText.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-export const TimelineItemSmallText = ({
-  children,
-}: {
-  children: ReactNode
-}) => {
-  return <div className='text-xs pt-1'>{children}</div>
-}

8f0ff8ef97d5a5eaa1ed758fad5b371cf54e32d2
rhmnaulia <rahmanauliax@gmail.com>
2024-03-06 14:34:08 +0700

    add home page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import { DocumentRenderer } from '@keystatic/core/renderer'
+import { Button } from '@/components/ui/button'
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <div className='flex flex-col gap-8'>
+      <section className='flex flex-col gap-2'>
+        <h1 className='text-2xl font-bold'>Aulia Rahman</h1>
+        <p className='text-base'>
+          <em>Crafting interfaces.</em> Building polished software and web
+          experiences. Experimenting with magical details in user interfaces.
+          Software Engineer at Indonesian GovTech Procurement.
+        </p>
+        <div className='flex gap-2 mt-2'>
+          <Button variant={'outline'}>writings</Button>
+          <Button variant={'outline'}>learn more about me</Button>
+        </div>
+      </section>
+    </div>
   )
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import Link from 'next/link'
+import { ModeToggle } from './theme-toggle'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
+
+export const NAV_MENU_LINK = [
+  {
+    label: 'Home',
+    path: '/',
+  },
+  {
+    label: 'Blog',
+    path: '/blog',
+  },
+  {
+    label: 'About',
+    path: '/about',
+  },
+]
+
+export function Navbar() {
+  let pathname = usePathname()
+
+  // Simplify pathname adjustment
+  pathname = pathname.startsWith('/blog/') ? '/blog' : pathname
+
+  function getLinkClassName(isActive: boolean) {
+    return cn(
+      isActive
+        ? 'dark:text-foreground font-medium'
+        : 'dark:text-foreground/60 text-foreground/80 transition-colors hover:text-foreground/100 hover:dark:text-foreground/90'
+    )
+  }
+
+  return (
+    <header className='z-40 flex w-full gap-2 flex-row justify-between items-center'>
+      <nav aria-label='Main navigation' className='flex items-center gap-4'>
+        {NAV_MENU_LINK.map((menu) => (
+          <Link
+            key={menu.path} // Use menu.path as a key for better uniqueness
+            href={menu.path}
+            className={getLinkClassName(pathname === menu.path)}
+            aria-label={menu.label} // Improve accessibility
+          >
+            {menu.label}
+          </Link>
+        ))}
+      </nav>
+      <ModeToggle />
+    </header>
+  )
+}
